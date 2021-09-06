1b37dcf8e38448eecde9b6c36905623635db42c2
nazgulnurtaeva1 <nazgulnurtaeva1@gmail.com>
2021-09-06 13:06:05 -0400

    the second commit

---
+++ src/gitFinalpack/gitclass0302.java
@@ -0,0 +1,12 @@
+package gitFinalpack;
+
+public class gitclass0302 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is the second commit file");
+		
+	}
+
+}
